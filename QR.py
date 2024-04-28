6fb1a661a52e427553b66de942d44ab140fa7526
LuisJop <luisjop7@gmail.com>
2024-04-28 15:53:11 -0600

    crear un apy para ingresar un link y que devuelva un QR

---
+++ QR.py
@@ -0,0 +1,12 @@
+import tkinter
+import qrcode
+
+
+window = tkinter.Tk()
+window.title("Link a QR")
+window.geometry('500x400')
+window.resizable(False, False)
+window.config(bg="gray")
+
+
+window.mainloop()
