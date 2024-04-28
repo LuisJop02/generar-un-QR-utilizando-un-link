a3ed8c4e6ed359988d429f7f18dd8c9b5067a25a
LuisJop <luisjop7@gmail.com>
2024-04-28 16:16:14 -0600

    se agrego la funcion y tkinter

--- QR.py
+++ QR.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import messagebox
 import qrcode
 
 
@@ -7,6 +8,28 @@ window.title("Link a QR")
 window.geometry('500x400')
 window.resizable(False, False)
 window.config(bg="gray")
+
+tkinter.Label(window, text='Link a QR', font="arial 15" ).place(x=200,y=5) 
+tkinter.Label(window, text="introduzca el link") .place(x=200,y=50)
+
+link = tkinter.StringVar()
+linkenter= tkinter.Entry(window, textvariable=link, width=50,  ).place(x=105,y=100)
+
+
+def QR():
+    texto = str(link.get())
+    img = qrcode.make(texto)
+    img.save("codigo_qr.png")
+    link.set("")
+    messagebox.showinfo( "generado","generado correctamente")
+
+
+
+tkinter.Button(window,text="generar QR", command=QR).place(x=210,y=130)
+
+
+
+
 
 
 window.mainloop()
