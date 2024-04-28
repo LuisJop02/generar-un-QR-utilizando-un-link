adc6d2c1ef41bdd9d9e5f4bfc2bc570d7fed62aa
LuisJop <luisjop7@gmail.com>
2024-04-28 16:53:03 -0600

    se agregaron nuevas funciones

--- QR.py
+++ QR.py
@@ -1,31 +1,45 @@
+import os
 import tkinter
-from tkinter import messagebox
+from tkinter import Label, messagebox
 import qrcode
+from PIL import Image, ImageTk
 
 
 window = tkinter.Tk()
 window.title("Link a QR")
-window.geometry('500x400')
+window.geometry('400x200')
 window.resizable(False, False)
 window.config(bg="gray")
 
-tkinter.Label(window, text='Link a QR', font="arial 15" ).place(x=200,y=5) 
-tkinter.Label(window, text="introduzca el link") .place(x=200,y=50)
+tkinter.Label(window, text='Link a QR', font="arial 15" ).place(x=150,y=5) 
+tkinter.Label(window, text="introduzca el link") .place(x=150,y=50)
 
-link = tkinter.StringVar()
-linkenter= tkinter.Entry(window, textvariable=link, width=50,  ).place(x=105,y=100)
+link1 = tkinter.StringVar()
+linkenter= tkinter.Entry(window, textvariable=link1, width=50,  ).place(x=40,y=100)
 
 
 def QR():
-    texto = str(link.get())
-    img = qrcode.make(texto)
-    img.save("codigo_qr.png")
-    link.set("")
-    messagebox.showinfo( "generado","generado correctamente")
+        link = str(link1.get())
+        qr = qrcode.QRCode(version=1, box_size=10, border=5)
+        qr.add_data(link)
+        link1.set("")
+        qr.make(fit=True)
+        img = qr.make_image(fill_color="black", back_color="white")
+        img = img.resize((200, 200))
+        qr_name = link.replace("://", "_").replace("/", "_").replace(".", "_") + ".png"
+        qr_path = os.path.join("qr_codes", qr_name)
+        img.save(qr_path)
+        
+        
+        messagebox.showinfo( "generado","generado correctamente")
 
 
 
-tkinter.Button(window,text="generar QR", command=QR).place(x=210,y=130)
+tkinter.Button(window,text="generar QR", command=QR).place(x=150,y=130)
+
+if __name__ == "__main__":
+    if not os.path.exists("qr_codes"):
+        os.makedirs("qr_codes")
 
 
 
